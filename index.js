bb45133b7d2e7e724c350a675788ddb6813317c0
Bre Humphres <breannahumphres@gmail.com>
2024-07-12 14:13:03 -0700

    arrays lab all passing

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats =  ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    name = cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    name = cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+name = [...cats, "Broom"];
+return name;
+}
+function prependCat(name) {
+    name = ["Arnold", ...cats];
+    return name;
+    }
+function removeLastCat() {
+   return cats.slice(0, - 1);
+} 
+function removeFirstCat() {
+    return cats.slice(1);
+ } 
